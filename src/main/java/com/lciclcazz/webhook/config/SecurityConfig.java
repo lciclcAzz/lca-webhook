077a1fe3915e2ca0e5f8f0c57107db59893b7250
hackingslab <hackingslab@gmail.com>
2017-07-02 18:48:21 +0700

    Edit - TemplateMessage type button

--- src/main/java/com/lciclcazz/webhook/config/SecurityConfig.java
+++ src/main/java/com/lciclcazz/webhook/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.lciclcazz.webhook.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import javax.inject.Inject;
 
@@ -24,7 +27,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
          		.withUser("user").password("user").roles("USER").and()
          		.withUser("admin").password("admin").roles("ADMIN");
     }
-	
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+				.antMatchers(HttpMethod.OPTIONS, "/**")
+				.antMatchers("/app/**/*.{js,html}")
+				.antMatchers("/static/**");
+	}
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
---
+++ src/main/java/com/lciclcazz/webhook/config/WebMvcConfiguration.java
@@ -0,0 +1,17 @@
+package com.lciclcazz.webhook.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by lciclcazz on 7/2/17.
+ */
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
